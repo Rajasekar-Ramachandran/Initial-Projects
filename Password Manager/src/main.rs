24d62a153934a05e2c2a501ee5ee7fb919644db0
Rajasekar-Ramachandran <rajasekar10902@gmail.com>
2022-11-20 18:03:41 +0530

    Created Token Recognizer

--- Password Manager/src/main.rs
+++ Password Manager/src/main.rs
@@ -1,32 +1,136 @@
 use std::fs::File;
-use std::io;
+use std::fs::OpenOptions;
 use std::io::prelude::*;
 
 fn main() {
 
-    let test_data = "Rajasekar";
-    println!("{}",encryption(test_data.to_string()));
-    println!("{}",decryption(encryption(test_data.to_string())));
-    create_file("test.txt","Dummy text");
-    let contents = read_file("test.txt");
+    let mut user_name = String::new();
+    //println!("{}",encryption(test_data.to_string()));
+    //println!("{}",decryption(encryption(test_data.to_string())));
+
+    create_file("passwords.txt");
+    write_file("[USERNAME:]\n");
+    let contents = read_file("passwords.txt");
     println!("{contents}");
-    while true {
+    let mut tokens = parser(contents);
+    let mut line = 0;
+   
+    for token in &tokens {
+        if token.0 == "USERNAME" && token.1 == ""{
+            println!("password-manager>Enter the user name : ");
+            std::io::stdin().read_line(&mut user_name).unwrap();
+            user_name = user_name.trim().to_string();
+            //tokens[0].1 = user_name;
+            //create_file("passwords.txt");
+            //write_file(tokens.to_string());
+        }
+        line = line + 1;
+    }
+    println!("{:?}",tokens);
+
+    loop {
         let mut command = String::new();
+        println!("password-manager>{}>",user_name);
         std::io::stdin().read_line(&mut command).unwrap();
         command = (&command.to_string().trim()).to_string();
         match command.as_str() {
             "exit" => {  break; }
+            "new user" => {
+                println!("Enter the user name : ");
+                std::io::stdin().read_line(&mut user_name).unwrap();
+                write_file(format!("[USERNAME:{}]",user_name.trim()).as_str());
+                println!("New user is created successfully!");
+            }
             &_ => { println!("Unable to find the command {command}.") }
         }
     }
+}
+
+// Parsing the raw string each by line
+// Token_Identifier will recognize the tokens and return them
+// 
+// @params contents : String
+// @return tokens : Vec<(String, String)>
+
+fn parser(contents: String) -> Vec<(String, String)>{
+
+    let mut tokens: Vec<(String, String)> = Vec::new();
+    let mut lines: Vec<char> = Vec::new();
+
+    // Iterating the raw string as a char
+    for chars in contents.chars() {
+
+        // Checking for new line sperator
+        // for spliting the lines
+        if chars == '\n' {
+
+            // Getting the key and value from the current line
+            tokens.push(token_identifier(&lines));
+            // Clearing the line for storing the next line
+            lines.clear();
+            continue; 
+        }
+
+        // Constructing the line until \n
+        lines.push(chars);
+    }
+
+    // @return
+    tokens
+}
+
+// Token identifier is used to identify the token.
+//
+// @params line : Vec<char>
+// @returns key : String, value : String
+
+fn token_identifier(line: &[char]) -> (String, String) {
+
+    // For token recognize
+    let mut start_token:bool = false;
+    let mut value_token:bool = false;
+
+    // For storing the key and value
+    let mut key = String::new();
+    let mut value = String::new();
+    
+    for character in line {
+
+        // Token recognizer
+        // If token is recognized then set the value as true
+        match character {
+            //'\n' => {break;}
+            '[' => {start_token = true; continue;}
+            ']' => {start_token = false;}
+            ':' => {value_token = true; continue;}
+            _=> {start_token = true;}
+        }
+
+        // Constructing the token
+        // As key and value
+        if start_token {
+            if value_token {
+                value = value + &character.to_string();
+                continue;
+            }
+            key = key + &character.to_string();
+        }
+    }
+    // @return
+    (key, value)
 }
 
 // Creating a file for storing the encrypted passwords
 //
 // @params Path, Content
 
-fn create_file(path: &str, content: &str) {
+fn create_file(path: &str) {
     let mut my_file = File::create(path).expect("Could not create the file");
+    my_file.write_all(b"[Encrypted File has been created]\n").expect("Failed to write into the file.");
+}
+
+fn write_file(content: &str) {
+    let mut my_file = OpenOptions::new().append(true).open("passwords.txt").expect("Failed to open the file");
     my_file.write_all(content.as_bytes()).expect("Failed to write into the file.");
 }
 
